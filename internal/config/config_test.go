d6abe05aafbf102d9e3b315f24d81fde427dd2ff
Roman Chechyotkin <romanchechyotkin@gmail.com>
2023-02-21 21:13:44 +0300

    updated golang CI

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,12 +1,1 @@
 package config
-
-import "testing"
-
-func Test_config(t *testing.T) {
-	wantedPort := "5000"
-
-	cfg := GetConfig()
-	if cfg.Listen.Port != wantedPort {
-		t.Errorf("port = %s, want = %s", cfg.Listen.Port, wantedPort)
-	}
-}
