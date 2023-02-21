a61dcccc4af7fa3b4a2832838bb90c423326af25
Roman Chechyotkin <romanchechyotkin@gmail.com>
2023-02-21 21:11:48 +0300

    updated golang CI

--- cmd/main/main_test.go
+++ cmd/main/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestPrint(t *testing.T) {
-	str := "Romsa"
+	str := "Roma"
 	wanted := "string = Roma"
 	if s := Print(str); s != wanted {
 		t.Errorf("Print() = %q, want = %q", s, wanted)
