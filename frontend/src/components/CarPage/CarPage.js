f69923732f057e49a9b049a0a41b5daecaca94dc
chechyotka <romanchechyotkin@gmail.com>
2023-09-18 17:25:04 +0300

    added reservation

--- frontend/src/components/CarPage/CarPage.js
+++ frontend/src/components/CarPage/CarPage.js
@@ -15,6 +15,12 @@ export const CarPage = () => {
     const [rate, setRate] = useState(1)
     const [reservations, setReservations] = useState([])
 
+    const [startHour, setStartHour] = useState("")
+    const [endHour, setEnbHour] = useState("")
+
+    const [startDate, setStartDate] = useState()
+    const [endDate, setEndDate] = useState()
+
     const fetchCarInfo = async () => {
         try {
             const res = await axiosInstance.get(`/cars/${params.id}`)
@@ -69,6 +75,35 @@ export const CarPage = () => {
         }
     }
 
+    const reserve = async () => {
+        const start = new Date(startDate)
+        const end = new Date(endDate)
+
+        let startDay = start.getDate()
+        let startMonth = start.getMonth()
+        let startYear = start.getFullYear()
+        console.log(startHour, startDay, startMonth+1, startYear)
+
+        let endDay = end.getDate()
+        let endMonth = end.getMonth()
+        let endYear = end.getFullYear()
+        console.log(endHour, endDay, endMonth+1, endYear)
+
+        try {
+            const res = await axiosInstance.post(`/cars/${params.id}/rent`, JSON.stringify({
+                "start_date": `${addZero(startHour)} ${addZero(startDay)}.${addZero(startMonth+1)}.${startYear}`,
+                "end_date": `${addZero(endHour)} ${addZero(endDay)}.${addZero(endMonth+1)}.${endYear}`,
+            }))
+            console.log(res)
+            window.location.reload()
+        } catch (e) {
+            console.log(e)
+        }
+
+
+
+    }
+
     return (
         <div className={"car_page"}>
             <div className={"car_info"}>
@@ -98,10 +133,33 @@ export const CarPage = () => {
             </div>
             <div className={"reservation"}>
                 <h2>reservation</h2>
+                <div className={"reservation_dates"}>       {/*    15 02.01.2006  */}
+                    <div className={"reservation_start"}>
+                        <label htmlFor={"start"}>start date</label>
+                        <input type="number" min={0} max={23} onChange={e => setStartHour(e.target.value)} />
+                        <input onChange={e => setStartDate(e.target.value)} type="date" id={"start"}/>
+                    </div>
+                    <div className={"reservation_end"}>
+                        <label htmlFor={"end"}>end date</label>
+                        <input type="number" min={0} max={23} onChange={e => setEnbHour(e.target.value)} />
+                        <input onChange={e => setEndDate(e.target.value)} type="date" id={"end"}/>
+                    </div>
+                    <button onClick={reserve}>ok</button>
+                </div>
                 {reservations !== "no reservations" ? reservations.map(r =>
-                    <div>Start:{r.start_date} End:{r.end_date}</div>
-                ) :  <div>no reservations</div>}
+                        <div key={r.start_date}>Start:{r.start_date} End:{r.end_date}</div>)
+                    :
+                    <div>no reservations</div>
+                }
             </div>
         </div>
     )
+}
+
+function addZero(num) {
+    if (String(num).length === 1) {
+        return `0${num}`
+    }
+
+    return `${num}`
 }
