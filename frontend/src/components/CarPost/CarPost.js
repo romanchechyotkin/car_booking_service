e8a8089228390ddcd0acb153da89f3fa74b4ebb1
Anastasia Novikova <anastasizzzs10@gmail.com>
2024-09-21 12:44:50 +0300

    create car page

--- frontend/src/components/CarPost/CarPost.js
+++ frontend/src/components/CarPost/CarPost.js
@@ -5,12 +5,37 @@ export const CarPost = (props) => {
     const {car, user_id} = props
 
     return (
-        <div key={car.id} className={"post"}>
-            <div>{car.brand} {car.model}</div>
-            <div className={"post_images"}>
-                <img src={STATIC+car.images[0]} alt="img"/>
+
+
+        // <div key={car.id} className={"post"}>
+        //     {/* <div>{car.brand} {car.model}</div>
+        //     <div className={"post_images"}>
+        //         <img src={STATIC+car.images[0]} alt="img"/>
+        //     </div>
+        //     <div>{car.pricePerDay}$</div> */}
+        
+
+        //     <div className="card">
+
+        //     </div>
+        // </div>
+
+        <div className="car-card">
+            <div className="car-card-header">
+                <h3>{car.brand} {car.model}</h3>
+                <span className="favorite-icon">❤️</span>
             </div>
-            <div>{car.pricePerDay}$</div>
+            <p className="car-type">{car.type}</p>
+            <img
+                src={car.images[0]} 
+                alt={`${car.brand} ${car.model}`}
+                className="car-image"
+            />
+            
+            <div className="car-card-footer">
+                <div className="price">${car.pricePerDay}/day</div> 
+                <button className="rent-button">Арендовать</button>
+            </div>
         </div>
     )
 }
